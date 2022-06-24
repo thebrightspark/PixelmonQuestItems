48c1723cfe71a275deddb841d371545f00ab5b2d
bright_spark <markpassey@btinternet.com>
2022-06-25 00:50:51 +0100

    Basic project setup

---
+++ src/main/java/brightspark/pixelmonquestitems/PixelmonQuestItems.java
@@ -0,0 +1,8 @@
+package brightspark.pixelmonquestitems;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(PixelmonQuestItems.MOD_ID)
+public class PixelmonQuestItems {
+	public static final String MOD_ID = "pixelmonquestitems";
+}
